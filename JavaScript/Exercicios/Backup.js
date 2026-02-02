826843ae5496ff72d56738752ac70e4ba98fa9f8
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2026-02-02 16:17:24 -0300

    Update Backup.js

--- JavaScript/Exercicios/Backup.js
+++ JavaScript/Exercicios/Backup.js
@@ -21,6 +21,8 @@ class Mesa {
     this.local = local;
   }
   
+  
+  //arrumar isso depois
   verificarLocal() {
     const locaisValidos = ['Salao', 'Jardim'];
     if (locaisValidos.includes(this.local)) {
@@ -41,6 +43,7 @@ class Reserva {
     this.qtdPessoas = qtdPessoas;
     this.localPreferencia = localPreferencia;
     this.periodo = periodo;
+    this.status = status || 'pendente';
   }
   
   verificarPeriodo() {
@@ -55,23 +58,24 @@ class Reserva {
   }
   
   
-  adicionarMesa(){
-    if(this.capacidade >= this.qtdPessoas){
-      Reserva.status = 'confirmado';
-      return 'confirmado'
-    } else if (this.capacidade < this.qtdPessoas){
-      Reserva.status = 'cancelado';
-      return 'cancelado'
-    } else {
-      Reserva.status = 'pendente';
-      return 'pendente'
-    }
+adicionarMesa(mesa) {
+  if (mesa.capacidade >= this.qtdPessoas) {
+    this.mesa = mesa; 
+    this.status = 'confirmado';
+    console.log(`Mesa ${mesa.numero} confirmada para ${this.cliente.nome}`);
+    return 'confirmado';
+  } else {
+    this.status = 'cancelado';
+    console.log("Mesa insuficiente para o número de pessoas.");
+    return 'cancelado';
   }
+}
+
 }
 
 const cliente1 = new Cliente('Luiz', '123.456.789-00');
 const restaurante1 = new Restaurante('CocoBambu', 'Avenida Kennedy, 700 Bairro Jardim do Mar, São Bernardo do Campo', 20, []);
-const mesa1 = new Mesa(1, 2, 'Salao');
+const mesa1 = new Mesa(1, 4, 'Salao');
 const reserva1 = new Reserva(cliente1, restaurante1, mesa1, 4, 'Salao', 'noite', '');
 
 console.log(reserva1);
